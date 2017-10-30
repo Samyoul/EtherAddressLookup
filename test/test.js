fb0d944a0e67a30d108ff4bf7ea58555e70a3eba
Samuel Hawksby-Robinson <samuel@samyoul.com>
2017-10-30 15:37:43 +0000

    Added dev dependencies and started to build out tests
    TODO fix:
    Currently getting a lot of bugs related to importing the EtherAddressLookup class. I don't know the causes, needs investigating

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
 var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
+var chai = require('chai');
+const chrome = require('sinon-chrome');
+var EtherAddressLookup = require('../js/DomManipulator.js');
+
+describe('Ether Address Lookup', function() {
+
+    before(function () {
+        global.chrome = chrome;
+    });
+
+    describe('DOM Manipulator', function() {
+
+        before(function(){
+           var objEtherAddressLookup = new EtherAddressLookup();
+        });
+
         it('should return -1 when the value is not present', function() {
             assert.equal(-1, [1,2,3].indexOf(4));
         });
     });
+
 });
